5aecbb68bfcac517e96149f37194b3bb44bebddf
udit <udit@gmal.com>
2023-07-12 15:35:44 +0530

    calv1

---
+++ cal.py
@@ -0,0 +1,20 @@
+num1=int(input("enter the first number="))
+num2=int(input("enter the second number="))
+operator=input("choose  + , - , * , / = ")
+if(operator=='+'):
+    total=num1+num2
+    print("total= "+ str(total))
+
+elif(operator=='-'):
+    total=num1-num2
+    print("total="+ str(total))    
+
+elif(operator=='*'):
+    total=num1-num2
+    print("total="+str(total))
+
+elif(operator=='/'):
+    total=num1/num2
+    print("total="+ str(total))
+else:
+    print("invalid input")
